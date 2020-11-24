8a67d7666415b3126d9ba6157173eaa895d404df
Matthieu Buffet <matthieu.buffet@ssi.gouv.fr>
2020-11-24 17:27:56 +0100

    Add first version of C bindings
    - Simplified API, does not expose internals like ArchiveWriterConfig,
      designed for C applications which need to generate encrypted
      archives in a simple manner.
    - mla.h generated through cbindgen, cbindgen.toml included
    - Test projects in C bundled for gcc and Visual Studio

---
+++ bindings/C/tests/windows-msvc/src/main.c
@@ -0,0 +1,88 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+#pragma comment(lib, "mla.lib")
+#include "mla.h"
+
+PCSTR szPubkey = "-----BEGIN PUBLIC KEY-----\n"
+   "MCowBQYDK2VwAyEA9md4yIIFx+ftwe0c1p2YsJFrobXWKxan54Bs+/jFagE=\n"
+   "-----END PUBLIC KEY-----\n";
+
+static int32_t callback_write(const uint8_t* pBuffer, uintptr_t length, void *context)
+{
+   fwrite(pBuffer, length, 1, (FILE*)context);
+   return 0;
+}
+
+static int32_t callback_flush(void *context)
+{
+   fflush((FILE*)context);
+   return 0;
+}
+
+int main()
+{
+   FILE* f = NULL;
+   if (fopen_s(&f, "test.mla", "w") != 0)
+   {
+      fprintf(stderr, " [!] Could not create output file\n");
+      return 1;
+   }
+
+   MLAStatus status = 0;
+   MLAConfigHandle hConfig = NULL;
+   status = mla_config_default_new(&hConfig);
+   if (status != MLA_STATUS_SUCCESS)
+   {
+      fprintf(stderr, " [!] Config creation failed with code %" PRIX64 "\n", (uint64_t)status);
+      return (int)status;
+   }
+
+   status = mla_config_add_public_keys(hConfig, szPubkey);
+   if (status != MLA_STATUS_SUCCESS)
+   {
+      fprintf(stderr, " [!] Public key set failed with code %" PRIX64 "\n", (uint64_t)status);
+      return (int)status;
+   }
+
+   MLAArchiveHandle hArchive = NULL;
+   status = mla_archive_new(&hConfig, &callback_write, &callback_flush, f, &hArchive);
+   if (status != MLA_STATUS_SUCCESS)
+   {
+      fprintf(stderr, " [!] Archive creation failed with code %" PRIX64 "\n", (uint64_t)status);
+      return (int)status;
+   }
+
+   MLAArchiveFileHandle hFile = NULL;
+   status = mla_archive_file_new(hArchive, "test.txt", &hFile);
+   if (status != MLA_STATUS_SUCCESS)
+   {
+      fprintf(stderr, " [!] File creation failed with code %" PRIX64 "\n", (uint64_t)status);
+      return 1;
+   }
+
+   status = mla_archive_file_append(hArchive, hFile, "Hello, World!\n", (uint32_t)strlen("Hello, World!\n"));
+   if (status != MLA_STATUS_SUCCESS)
+   {
+      fprintf(stderr, " [!] File write failed with code %" PRIX64 "\n", (uint64_t)status);
+      return 1;
+   }
+
+   status = mla_archive_file_close(hArchive, &hFile);
+   if (status != MLA_STATUS_SUCCESS)
+   {
+      fprintf(stderr, " [!] File close failed with code %" PRIX64 "\n", (uint64_t)status);
+      return 1;
+   }
+
+   status = mla_archive_close(&hArchive);
+   if (status != MLA_STATUS_SUCCESS)
+   {
+      fprintf(stderr, " [!] Archive close failed with code %" PRIX64 "\n", (uint64_t)status);
+      return 1;
+   }
+
+   fclose(f);
+   return 0;
+}
