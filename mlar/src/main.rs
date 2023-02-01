5e64db925e01e657ecdac8f2394e10ba882c117c
Camille Mougey <camille.mougey@ssi.gouv.fr>
2023-02-01 08:43:45 +0100

    Use a LRU cache for linear extraction

--- mlar/src/main.rs
+++ mlar/src/main.rs
@@ -19,15 +19,19 @@ use mla::{
 use rand::SeedableRng;
 use rand_chacha::ChaChaRng;
 use sha2::{Digest, Sha512};
+use std::cell::RefCell;
 use std::collections::{HashMap, HashSet};
 use std::error;
 use std::fmt;
 use std::fs::{self, read_dir, File};
 use std::io::{self, BufRead};
 use std::io::{Read, Seek, Write};
+use std::num::NonZeroUsize;
 use std::path::{Component, Path, PathBuf};
 use tar::{Builder, Header};
 use zeroize::Zeroize;
+
+use lru::LruCache;
 
 // ----- Error ------
 
@@ -443,19 +447,29 @@ fn create_file<P1: AsRef<Path>>(
 ///
 /// This wrapper is used to avoid opening all files simultaneously, potentially
 /// reaching the filesystem limit, but rather appending to file on-demand
-/// This could be enhanced with a limited pool of active file, but this
-/// optimisation doesn't seems necessary for now
-struct FileWriter {
+/// 
+/// A limited pool of active file, in a LRU cache, is used to avoid too many open-close
+struct FileWriter<'a> {
     /// Target file for data appending
     path: PathBuf,
+    cache: &'a RefCell<LruCache<PathBuf, File>>,
 }
 
-impl Write for FileWriter {
+/// Max number of fd simultaneously opened
+pub const FILE_WRITER_POOL_SIZE: usize = 1000;
+
+impl<'a> Write for FileWriter<'a> {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        fs::OpenOptions::new()
+        let mut cache = self.cache.borrow_mut();
+        if !cache.contains(&self.path) {
+            let file = fs::OpenOptions::new()
             .append(true)
-            .open(&self.path)?
-            .write(buf)
+            .open(&self.path)?; 
+            cache.put(self.path.clone(), file);
+        }
+        // Safe to `unwrap` here cause we ensure the element is in the cache
+        let file = cache.get_mut(&self.path).unwrap();
+        file.write(buf)
     }
 
     fn flush(&mut self) -> io::Result<()> {
@@ -575,11 +589,12 @@ fn extract(matches: &ArgMatches) -> Result<(), MlarError> {
         if verbose {
             println!("Extracting the whole archive using a linear extraction");
         }
+        let cache = RefCell::new(LruCache::new(NonZeroUsize::new(FILE_WRITER_POOL_SIZE).unwrap()));
         let mut export: HashMap<&String, FileWriter> = HashMap::new();
         for fname in &iter {
             match create_file(&output_dir, fname)? {
                 Some((_file, path)) => {
-                    export.insert(fname, FileWriter { path });
+                    export.insert(fname, FileWriter { path, cache: &cache });
                 }
                 None => continue,
             }
