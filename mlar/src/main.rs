8b163a02b3e7355c88a38a664c63c36e6892d766
extiop <29679238+extiop@users.noreply.github.com>
2025-05-27 01:34:00 +0200

    feat(mlar): output private keys as PEM by default, like public keys

--- mlar/src/main.rs
+++ mlar/src/main.rs
@@ -837,9 +837,9 @@ fn keygen(matches: &ArgMatches) -> Result<(), MlarError> {
         .write_all(key_pair.public_as_pem().as_bytes())
         .expect("Error writing the public key");
 
-    // Output the private key in DER format, to avoid common mistakes
+    // Output the private key in PEM format, to ease integration in text based
     output_priv
-        .write_all(&key_pair.private_der)
+        .write_all(&key_pair.private_as_pem().as_bytes())
         .expect("Error writing the private key");
     Ok(())
 }
