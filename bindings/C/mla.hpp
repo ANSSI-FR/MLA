e039ba4ffea994cbab4ef99a9c411a8c3c900c4b
extiop <29679238+extiop@users.noreply.github.com>
2025-05-27 16:20:18 +0200

    feat(mla-bindings-c): add DER or PEM API for keys

--- bindings/C/mla.hpp
+++ bindings/C/mla.hpp
@@ -108,6 +108,12 @@ MLAStatus mla_config_default_new(MLAConfigHandle *handle_out);
 /// (referenced by the handle returned by mla_config_default_new()).
 MLAStatus mla_config_add_public_keys(MLAConfigHandle config, const char *public_keys);
 
+/// Appends the given public key(s) in DER format to an existing given configuration
+/// (referenced by the handle returned by mla_config_default_new()).
+MLAStatus mla_config_add_public_keys_der(MLAConfigHandle config,
+                                         const uint8_t *public_keys_data,
+                                         uintptr_t public_keys_len);
+
 /// Sets the compression level in an existing given configuration
 /// (referenced by the handle returned by mla_config_default_new()).
 /// Currently this level can only be an integer N with 0 <= N <= 11,
@@ -117,11 +123,16 @@ MLAStatus mla_config_set_compression_level(MLAConfigHandle config, uint32_t leve
 /// Create an empty ReaderConfig
 MLAStatus mla_reader_config_new(MLAConfigHandle *handle_out);
 
-/// Appends the given private key to an existing given configuration
+/// Appends the given private key in DER format to an existing given configuration
 /// (referenced by the handle returned by mla_reader_config_new()).
-MLAStatus mla_reader_config_add_private_key(MLAConfigHandle config,
-                                            const uint8_t *private_key_data,
-                                            uintptr_t private_key_len);
+MLAStatus mla_reader_config_add_private_key_der(MLAConfigHandle config,
+                                                const uint8_t *private_key_data,
+                                                uintptr_t private_key_len);
+
+/// Appends the given private key in PEM format to an existing given configuration
+/// (referenced by the handle returned by mla_reader_config_new()).
+MLAStatus mla_reader_config_add_private_key_pem(MLAConfigHandle config,
+                                                const char *private_key_pem);
 
 /// Open a new MLA archive using the given configuration, which is consumed and freed
 /// (its handle cannot be reused to create another archive). The archive is streamed
