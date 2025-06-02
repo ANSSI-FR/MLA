92a58f3fd934efb1041dfbcf1829901ef4efe548
extiop <29679238+extiop@users.noreply.github.com>
2025-06-02 12:55:14 +0200

    feat(mla-bindings-py): adapt tests as path read and DER parsing by default constructor are removed

--- bindings/python/tests/test_mla.py
+++ bindings/python/tests/test_mla.py
@@ -13,6 +13,7 @@ FILES = {
     "file2": b"DATA_2",
 }
 
+
 @pytest.fixture
 def basic_archive():
     "Create a temporary archive and return its path"
@@ -23,10 +24,12 @@ def basic_archive():
     archive.finalize()
     return fname
 
+
 def test_layers_bitflag_export():
     assert mla.LAYER_DEFAULT == mla.LAYER_COMPRESS | mla.LAYER_ENCRYPT
     assert mla.LAYER_EMPTY == 0
     assert mla.LAYER_DEFAULT != mla.LAYER_EMPTY
+
 
 def test_bad_mode():
     "Ensure MLAFile with an unknown mode raise an error"
@@ -37,12 +40,14 @@ def test_bad_mode():
     with pytest.raises(FileNotFoundError):
         open(target_file)
 
+
 def test_repr():
     "Ensure the repr is correct"
     path = tempfile.mkstemp(suffix=".mla")[1]
     archive = MLAFile(path, "w")
     assert repr(archive) == "<MLAFile path='%s' mode='w'>" % path
     archive.finalize()
+
 
 def test_forbidden_in_write_mode():
     "Ensure some API cannot be called in write mode"
@@ -51,15 +56,15 @@ def test_forbidden_in_write_mode():
     # .keys
     with pytest.raises(mla.BadAPIArgument):
         archive.keys()
-    
+
     # __contains__
     with pytest.raises(mla.BadAPIArgument):
         "name" in archive
-    
+
     # __getitem__
     with pytest.raises(mla.BadAPIArgument):
         archive["name"]
-    
+
     # __len__
     with pytest.raises(mla.BadAPIArgument):
         len(archive)
@@ -67,6 +72,7 @@ def test_forbidden_in_write_mode():
     # list_files
     with pytest.raises(mla.BadAPIArgument):
         archive.list_files()
+
 
 def test_forbidden_in_read_mode(basic_archive):
     "Ensure some API cannot be called in write mode"
@@ -79,6 +85,7 @@ def test_forbidden_in_read_mode(basic_archive):
     # .finalize
     with pytest.raises(mla.BadAPIArgument):
         archive.finalize()
+
 
 def test_read_api(basic_archive):
     "Test basics read APIs"
@@ -100,6 +107,7 @@ def test_read_api(basic_archive):
     # __len__
     assert len(archive) == 2
 
+
 def test_list_files(basic_archive):
     "Test list files possibilities"
     archive = MLAFile(basic_archive)
@@ -108,25 +116,38 @@ def test_list_files(basic_archive):
     assert sorted(archive.list_files()) == sorted(list(FILES.keys()))
 
     # With size
-    assert sorted([
-        (filename, info.size) for filename, info in archive.list_files(include_size=True).items()
-    ]) == sorted([
-        (filename, len(data)) for filename, data in FILES.items()
-    ])
+    assert sorted(
+        [
+            (filename, info.size)
+            for filename, info in archive.list_files(include_size=True).items()
+        ]
+    ) == sorted([(filename, len(data)) for filename, data in FILES.items()])
 
     # With hash
-    assert sorted([
-        (filename, info.hash) for filename, info in archive.list_files(include_hash=True).items()
-    ]) == sorted([
-        (filename, hashlib.sha256(data).digest()) for filename, data in FILES.items()
-    ])
+    assert sorted(
+        [
+            (filename, info.hash)
+            for filename, info in archive.list_files(include_hash=True).items()
+        ]
+    ) == sorted(
+        [(filename, hashlib.sha256(data).digest()) for filename, data in FILES.items()]
+    )
 
     # With size and hash
-    assert sorted([
-        (filename, info.size, info.hash) for filename, info in archive.list_files(include_size=True, include_hash=True).items()
-    ]) == sorted([
-        (filename, len(data), hashlib.sha256(data).digest()) for filename, data in FILES.items()
-    ])
+    assert sorted(
+        [
+            (filename, info.size, info.hash)
+            for filename, info in archive.list_files(
+                include_size=True, include_hash=True
+            ).items()
+        ]
+    ) == sorted(
+        [
+            (filename, len(data), hashlib.sha256(data).digest())
+            for filename, data in FILES.items()
+        ]
+    )
+
 
 def test_write_api():
     "Test basics write APIs"
@@ -146,13 +167,15 @@ def test_write_api():
     assert archive["file1"] == FILES["file1"]
     assert archive["file2"] == FILES["file2"]
 
+
 def test_double_write():
     "Rewriting the file must raise an MLA error"
     archive = MLAFile(tempfile.mkstemp(suffix=".mla")[1], "w")
     archive["file1"] = FILES["file1"]
-    
+
     with pytest.raises(mla.DuplicateFilename):
         archive["file1"] = FILES["file1"]
+
 
 def test_context_read(basic_archive):
     "Test reading using a `with` statement (context management protocol)"
@@ -161,18 +184,20 @@ def test_context_read(basic_archive):
         for name, data in FILES.items():
             assert mla[name] == data
 
+
 def test_context_write():
     "Test writing using a `with` statement (context management protocol)"
     path = tempfile.mkstemp(suffix=".mla")[1]
     with MLAFile(path, "w") as mla:
         for name, data in FILES.items():
             mla[name] = data
-    
+
     # Check the resulting file
     with MLAFile(path) as mla:
         assert sorted(mla.keys()) == sorted(list(FILES.keys()))
         for name, data in FILES.items():
             assert mla[name] == data
+
 
 def test_context_write_error():
     "Raise an error during the context write __exit__"
@@ -182,6 +207,7 @@ def test_context_write_error():
             # .finalize will be called twice, causing an exception
             archive.finalize()
 
+
 def test_context_write_error_in_with():
     "Raise an error in the with statement, it must be re-raised"
     CustomException = type("CustomException", (Exception,), {})
@@ -189,6 +215,7 @@ def test_context_write_error_in_with():
         with MLAFile(tempfile.mkstemp(suffix=".mla")[1], "w") as mla:
             # INTENTIONNALY BUGGY
             raise CustomException
+
 
 def test_writer_config_layers():
     "Test writer config creation for layers"
@@ -211,27 +238,26 @@ def test_writer_config_layers():
     # Check for error on unknown layer (0xFF)
     with pytest.raises(mla.BadAPIArgument):
         config.enable_layer(0xFF)
-    
+
     with pytest.raises(mla.BadAPIArgument):
         config.disable_layer(0xFF)
-    
+
     with pytest.raises(mla.BadAPIArgument):
         config.set_layers(0xFF)
-    
+
     with pytest.raises(mla.BadAPIArgument):
         config = mla.WriterConfig(layers=0xFF)
-    
+
     # Chaining
-    config = mla.WriterConfig().enable_layer(
-            mla.LAYER_COMPRESS
-        ).enable_layer(
-            mla.LAYER_ENCRYPT
-        ).disable_layer(
-            mla.LAYER_COMPRESS
-        ).set_layers(
-            mla.LAYER_COMPRESS
-        )
+    config = (
+        mla.WriterConfig()
+        .enable_layer(mla.LAYER_COMPRESS)
+        .enable_layer(mla.LAYER_ENCRYPT)
+        .disable_layer(mla.LAYER_COMPRESS)
+        .set_layers(mla.LAYER_COMPRESS)
+    )
     assert config.layers == mla.LAYER_COMPRESS
+
 
 def test_writer_config_compression():
     "Test compression API in WriterConfig creation"
@@ -240,7 +266,7 @@ def test_writer_config_compression():
         config.with_compression_level(-1)
     with pytest.raises(mla.ConfigError):
         config.with_compression_level(0xFF)
-    
+
     # Value
     config.with_compression_level(mla.DEFAULT_COMPRESSION_LEVEL)
     assert config.compression_level == mla.DEFAULT_COMPRESSION_LEVEL
@@ -251,97 +277,86 @@ def test_writer_config_compression():
     out = config.with_compression_level(mla.DEFAULT_COMPRESSION_LEVEL)
     assert out is config
 
+
 # Expected: mla/bindings/python/tests/
 MLA_BASE_PATH = os.path.dirname(
-    os.path.dirname(
-        os.path.dirname(
-            os.path.dirname(
-                __file__
-            )
-        )
-    )
+    os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 )
 SAMPLE_PATH = os.path.join(MLA_BASE_PATH, "samples")
+
 
 def test_public_keys():
     "Test the PublicKeys object"
     # Bad parsing
     with pytest.raises(mla.InvalidKeyFormat):
         mla.PublicKeys(b"NOT A KEY")
-    
-    with pytest.raises(FileNotFoundError):
-        mla.PublicKeys("/tmp/does_not_exists")
-    
-    # Open a PEM key, through path
-    pkeys_pem = mla.PublicKeys(os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem"))
-    assert len(pkeys_pem.keys) == 1
-    
-    # Open a DER key, through path
-    pkeys_der = mla.PublicKeys(os.path.join(SAMPLE_PATH, "test_mlakey_pub.der"))
-    assert len(pkeys_pem.keys) == 1
 
-    # Keys must be the same
-    assert pkeys_pem.keys == pkeys_der.keys
+    # This is a private key, not a public one
+    with pytest.raises(mla.InvalidKeyFormat):
+        mla.PublicKeys(os.path.join(SAMPLE_PATH, "test_mlakey.pem"))
 
     # Open a PEM key, through data
-    pkeys_pem = mla.PublicKeys(open(os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem"), "rb").read())
+    pkeys_pem = mla.PublicKeys(
+        open(os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem"), "rb").read()
+    )
     assert len(pkeys_pem.keys) == 1
-    
+
     # Open a DER key, through data
-    pkeys_pem = mla.PublicKeys(open(os.path.join(SAMPLE_PATH, "test_mlakey_pub.der"), "rb").read())
+    pkeys_der = mla.PublicKeys.from_der(
+        open(os.path.join(SAMPLE_PATH, "test_mlakey_pub.der"), "rb").read()
+    )
     assert len(pkeys_pem.keys) == 1
-    
+
     # Keys must be the same
     assert pkeys_pem.keys == pkeys_der.keys
 
-    # Open several keys, using both path and data
-    pkeys =  mla.PublicKeys(
-        os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem"),
-        open(os.path.join(SAMPLE_PATH, "test_mlakey_2_pub.pem"), "rb").read()
+    # Open several PEM keys, using data
+    pkeys = mla.PublicKeys(
+        open(os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem"), "rb").read(),
+        open(os.path.join(SAMPLE_PATH, "test_mlakey_2_pub.pem"), "rb").read(),
     )
     assert len(pkeys.keys) == 2
+
 
 def test_private_keys():
     "Test the PrivateKeys object"
     # Bad parsing
     with pytest.raises(mla.InvalidKeyFormat):
         mla.PrivateKeys(b"NOT A KEY")
-    
+
+    # This is a public key, not a private one
     with pytest.raises(mla.InvalidKeyFormat):
-        # This is a public key, not a private one
         mla.PrivateKeys(os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem"))
 
-    with pytest.raises(FileNotFoundError):
-        mla.PrivateKeys("/tmp/does_not_exists")
-    
-    # Open a PEM key, through path
-    pkeys_pem = mla.PrivateKeys(os.path.join(SAMPLE_PATH, "test_mlakey.pem"))
-    assert len(pkeys_pem.keys) == 1
-    
-    # Open a DER key, through path
-    pkeys_der = mla.PrivateKeys(os.path.join(SAMPLE_PATH, "test_mlakey.der"))
-    assert len(pkeys_pem.keys) == 1
-
-    # Keys must be the same
-    assert pkeys_pem.keys == pkeys_der.keys
-
     # Open a PEM key, through data
-    pkeys_pem = mla.PrivateKeys(open(os.path.join(SAMPLE_PATH, "test_mlakey.pem"), "rb").read())
+    pkeys_pem = mla.PrivateKeys(
+        open(os.path.join(SAMPLE_PATH, "test_mlakey.pem"), "rb").read()
+    )
     assert len(pkeys_pem.keys) == 1
-    
+
     # Open a DER key, through data
-    pkeys_pem = mla.PrivateKeys(open(os.path.join(SAMPLE_PATH, "test_mlakey.der"), "rb").read())
+    pkeys_der = mla.PrivateKeys.from_der(
+        open(os.path.join(SAMPLE_PATH, "test_mlakey.der"), "rb").read()
+    )
     assert len(pkeys_pem.keys) == 1
-    
+
     # Keys must be the same
     assert pkeys_pem.keys == pkeys_der.keys
 
-    # Open several keys, using both path and data
-    pkeys =  mla.PrivateKeys(
-        os.path.join(SAMPLE_PATH, "test_mlakey.pem"),
-        open(os.path.join(SAMPLE_PATH, "test_mlakey_2.der"), "rb").read()
+    # Open several PEM keys, using data
+    pkeys = mla.PrivateKeys(
+        open(os.path.join(SAMPLE_PATH, "test_mlakey.pem"), "rb").read(),
+        open(os.path.join(SAMPLE_PATH, "test_mlakey_2.pem"), "rb").read(),
     )
     assert len(pkeys.keys) == 2
+
+    # Open several DER keys, using data
+    pkeys = mla.PrivateKeys.from_der(
+        open(os.path.join(SAMPLE_PATH, "test_mlakey.der"), "rb").read(),
+        open(os.path.join(SAMPLE_PATH, "test_mlakey_2.der"), "rb").read(),
+    )
+    assert len(pkeys.keys) == 2
+
 
 def test_writer_config_public_keys():
     "Test public keys API in WriterConfig creation"
@@ -350,33 +365,38 @@ def test_writer_config_public_keys():
     config = mla.WriterConfig()
     with pytest.raises(mla.InvalidKeyFormat):
         config.set_public_keys(mla.PublicKeys(b"NOT A KEY"))
-    
+
     # Test shortcut on object build
     config = mla.WriterConfig(
-        public_keys=mla.PublicKeys(os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem"))
+        public_keys=mla.PublicKeys(
+            open(os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem"), "rb").read()
+        )
     )
     # Test the getter
     assert len(config.public_keys.keys) == 1
 
     # Chaining
-    out = config.set_public_keys(mla.PublicKeys(
-        os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem"),
-        open(os.path.join(SAMPLE_PATH, "test_mlakey_2_pub.pem"), "rb").read()
-    ))
+    out = config.set_public_keys(
+        mla.PublicKeys(
+            open(os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem"), "rb").read(),
+            open(os.path.join(SAMPLE_PATH, "test_mlakey_2_pub.pem"), "rb").read(),
+        )
+    )
     assert out is config
     assert len(config.public_keys.keys) == 2
+
 
 def test_mlafile_bad_config():
     "Try to create a MLAFile with the wrong config parameter"
     with pytest.raises(TypeError):
         MLAFile(tempfile.mkstemp(suffix=".mla")[1], "w", config="NOT A CONFIG")
-    
+
     with pytest.raises(TypeError):
         MLAFile(tempfile.mkstemp(suffix=".mla")[1], "w", config=mla.ReaderConfig())
-    
+
     with pytest.raises(TypeError):
         MLAFile(tempfile.mkstemp(suffix=".mla")[1], "r", config=mla.WriterConfig())
-    
+
 
 def test_reader_config_api():
     "Test the ReaderConfig API"
@@ -385,47 +405,72 @@ def test_reader_config_api():
     assert config.private_keys is None
 
     config.set_private_keys(
-        mla.PrivateKeys(os.path.join(SAMPLE_PATH, "test_mlakey.pem"))
+        mla.PrivateKeys(open(os.path.join(SAMPLE_PATH, "test_mlakey.pem"), "rb").read())
     )
     assert len(config.private_keys.keys) == 1
 
-    config = mla.ReaderConfig(private_keys=mla.PrivateKeys(os.path.join(SAMPLE_PATH, "test_mlakey.pem")))
+    config = mla.ReaderConfig(
+        private_keys=mla.PrivateKeys(
+            open(os.path.join(SAMPLE_PATH, "test_mlakey.pem"), "rb").read()
+        )
+    )
     assert len(config.private_keys.keys) == 1
 
     # Chaining
     config = mla.ReaderConfig()
     out = config.set_private_keys(
-        mla.PrivateKeys(os.path.join(SAMPLE_PATH, "test_mlakey.pem")),
+        mla.PrivateKeys(
+            open(os.path.join(SAMPLE_PATH, "test_mlakey.pem"), "rb").read()
+        ),
     )
     assert out is config
+
 
 def test_write_then_read_encrypted():
     "Create an encrypted archive, then read it"
     # Create the archive
     path = tempfile.mkstemp(suffix=".mla")[1]
-    with MLAFile(path, "w", config=mla.WriterConfig(
-        public_keys=mla.PublicKeys(os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem")),
-        layers=mla.LAYER_ENCRYPT
-    )) as archive:
+    with MLAFile(
+        path,
+        "w",
+        config=mla.WriterConfig(
+            public_keys=mla.PublicKeys(
+                open(os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem"), "rb").read()
+            ),
+            layers=mla.LAYER_ENCRYPT,
+        ),
+    ) as archive:
         for name, data in FILES.items():
             archive[name] = data
-    
+
     # Read the archive
-    with MLAFile(path, config=mla.ReaderConfig(
-        private_keys=mla.PrivateKeys(os.path.join(SAMPLE_PATH, "test_mlakey.pem"))
-    )) as archive:
+    with MLAFile(
+        path,
+        config=mla.ReaderConfig(
+            private_keys=mla.PrivateKeys(
+                open(os.path.join(SAMPLE_PATH, "test_mlakey.pem"), "rb").read()
+            )
+        ),
+    ) as archive:
         assert sorted(archive.keys()) == sorted(list(FILES.keys()))
         for name, data in FILES.items():
             assert archive[name] == data
+
 
 def test_read_encrypted_archive_bad_key():
     "Try to read an encrypted archive with a bad key"
     # Create the archive
     path = tempfile.mkstemp(suffix=".mla")[1]
-    with MLAFile(path, "w", config=mla.WriterConfig(
-        public_keys=mla.PublicKeys(os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem")),
-        layers=mla.LAYER_ENCRYPT
-    )) as archive:
+    with MLAFile(
+        path,
+        "w",
+        config=mla.WriterConfig(
+            public_keys=mla.PublicKeys(
+                open(os.path.join(SAMPLE_PATH, "test_mlakey_pub.pem"), "rb").read()
+            ),
+            layers=mla.LAYER_ENCRYPT,
+        ),
+    ) as archive:
         for name, data in FILES.items():
             archive[name] = data
 
@@ -433,13 +478,19 @@ def test_read_encrypted_archive_bad_key():
     with pytest.raises(mla.PrivateKeyNeeded):
         with MLAFile(path) as archive:
             pass
-    
+
     # Try to read with an incorrect key (mla.ConfigError: PrivateKeyNotFound)
     with pytest.raises(mla.ConfigError):
-        with MLAFile(path, config=mla.ReaderConfig(
-            private_keys=mla.PrivateKeys(os.path.join(SAMPLE_PATH, "test_mlakey_2.der"))
-        )) as archive:
+        with MLAFile(
+            path,
+            config=mla.ReaderConfig(
+                private_keys=mla.PrivateKeys.from_der(
+                    open(os.path.join(SAMPLE_PATH, "test_mlakey_2.der"), "rb").read()
+                )
+            ),
+        ) as archive:
             pass
+
 
 def test_write_file_to_str(basic_archive):
     """Test archive.write_file_to(), using the String output version"""
@@ -453,6 +504,7 @@ def test_write_file_to_str(basic_archive):
     # Check the files
     for name, data in FILES.items():
         assert open(os.path.join(tmpdir, name), "rb").read() == data
+
 
 def test_write_file_to_file(basic_archive):
     """Test archive.write_file_to(), using the File output version"""
@@ -478,11 +530,11 @@ class BytesIOCounter(io.BytesIO):
         super().__init__(*args, **kwargs)
         self.write_count = 0
         self.read_count = 0
-    
+
     def write(self, *args, **kwargs):
         self.write_count += 1
         return super().write(*args, **kwargs)
-    
+
     def read(self, *args, **kwargs):
         self.read_count += 1
         return super().read(*args, **kwargs)
@@ -509,6 +561,7 @@ def test_write_file_to_file_chunk_size(basic_archive):
         output.seek(0)
         assert output.read() == FILES["file1"]
 
+
 def test_add_file_from_str():
     "Test archive.add_file_from(), using the String input version"
     # Create the archive
@@ -528,6 +581,7 @@ def test_add_file_from_str():
         for name, data in FILES.items():
             assert archive[name] == data
 
+
 def test_add_file_from_io():
     "Test archive.add_file_from(), using the IO input version"
     # Create the archive
@@ -545,9 +599,10 @@ def test_add_file_from_io():
         for name, data in FILES.items():
             assert archive[name] == data
 
+
 def test_add_file_from_io_chunk_size():
     "Test archive.add_file_from(), using the IO input version"
-    for chunk_size in [1, 2]:    
+    for chunk_size in [1, 2]:
         # Create the archive
         path = tempfile.mkstemp(suffix=".mla")[1]
         data = FILES["file1"]
@@ -566,5 +621,3 @@ def test_add_file_from_io_chunk_size():
         # Read the archive
         with MLAFile(path) as archive:
             assert archive["file1"] == data
-
-    
