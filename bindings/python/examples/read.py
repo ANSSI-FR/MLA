eb257d2a3bd70009acbe80570ea44ad52865ca2a
extiop <29679238+extiop@users.noreply.github.com>
2025-08-06 19:11:35 +0200

    doc(mla-bindings-py): add documentation for usage and examples

---
+++ bindings/python/examples/read.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import mla
+
+def main():
+    sig_cfg = mla.SignatureConfig.without_signature_verification()
+    config = mla.ReaderConfig.without_encryption(sig_cfg)
+    with mla.MLAFile("example.mla", "r", config) as archive:
+        for name in archive.keys():
+            print(f"{name.raw_content_to_escaped_string()}: {archive[name]}")
+
+if __name__ == "__main__":
+    main()
